93f8f70cfd04b259e7cfa31ddaf4cdc032c93cdf
John Neville <johnpneville@gmail.com>
2026-01-07 14:46:57 -0500

    Update PowerFeather-SDK version to 1.1.1

--- components/powerfeather_mainboard/__init__.py
+++ components/powerfeather_mainboard/__init__.py
@@ -80,7 +80,7 @@ async def to_code(config):
     mainboard = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(mainboard, config)
 
-    cg.add_library(name="PowerFeather-SDK", version="^1.1.0")
+    cg.add_library(name="PowerFeather-SDK", version="^1.1.1")
 
     if battery_capacity_config := config.get(CONF_BATTERY_CAPACITY):
         cg.add(mainboard.set_battery_capacity(battery_capacity_config))
